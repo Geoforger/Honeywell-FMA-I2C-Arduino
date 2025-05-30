5c636058e5423cfa6d0526c316d3585dd994a488
George Brayshaw <brayshaw.george@gmail.com>
2025-05-30 14:41:24 +0100

    Class w/ skeleton methods

--- FMA_I2C.cpp
+++ FMA_I2C.cpp
@@ -15,13 +15,35 @@
 // Libraries
 //--------------------------------------------------------------------------------------------------
 #include <Wire.h> // I2C library
-#include "FMAMSDXX005WC2C3.h"
+#include "FMA_I2C.h"
 
 //--------------------------------------------------------------------------------------------------
 // Global variables and object definitions
 //--------------------------------------------------------------------------------------------------
+HoneyWellFMA_I2C::HoneyWellFMA_I2C() {
+    // Constructor
+}
 
+void HoneyWellFMA_I2C::init(int address, char transfer_function) {
+    // Set the I2C address and transfer function
+    i2c_address = address;
+    transfer_function = transfer_function;
 
-//--------------------------------------------------------------------------------------------------
-// Functions
-//--------------------------------------------------------------------------------------------------
+    Wire.begin(); // Initialize I2C communication
+}
+
+float HoneyWellFMA_I2C::read_sensor() {
+
+}
+
+void HoneyWellFMA_I2C::calibrate_sensor(int iterations) {
+
+}
+
+float HoneyWellFMA_I2C::transfer_function_A(uint16_t raw_data) {
+    
+}
+
+float HoneyWellFMA_I2C::transfer_function_C(uint16_t raw_data) {
+    
+}
